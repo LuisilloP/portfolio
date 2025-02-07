b3e33b9cd286478b4f2e729680d125f6fdfa3150
LuisilloP <luisaraya98@hotmail.com>
2025-02-07 17:14:00 -0300

    v01

---
+++ src/Interfaces/Interfaces.ts
@@ -0,0 +1,9 @@
+interface ProyectInterface {
+    name: string;
+    image: string;
+    text: string;
+    tecnologies: string[];
+    url: string;
+    img_position: "default" | "reverse";
+  }
+export type {ProyectInterface}
